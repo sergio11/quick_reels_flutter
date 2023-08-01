b3bc0afdd2dc92e43d61727e3556cac64e043774
Sergio Sánchez <sss4esob@gmail.com>
2023-08-01 18:01:22 +0200

    Configure Firebase android integration

--- lib/view/screens/signup_screen.dart
+++ lib/view/screens/signup_screen.dart
@@ -19,7 +19,7 @@ class SignupScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'Tiktok Clone',
+              'Quick Reels',
               style: TextStyle(
                 fontSize: 35,
                 color: buttonColor,
