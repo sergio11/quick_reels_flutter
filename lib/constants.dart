71ce69587b4bf0890da03e9483d37c8ea2c17050
Sergio Sánchez <sss4esob@gmail.com>
2023-08-01 17:10:57 +0200

    Implement Home screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,6 +4,13 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:quickreels/controllers/auth_controller.dart';
 
+import 'view/screens/profile_screen.dart';
+
+List pages = [
+  Text('Messages Screen'),
+  ProfileScreen(uid: authController.user.uid),
+];
+
 // COLORS
 const backgroundColor = Colors.black;
 var buttonColor = Colors.red[400];
