ea610cbbba0021008c30a85dda0c2f84ba6cfc12
Sergio Sánchez <sss4esob@gmail.com>
2023-08-01 17:05:49 +0200

    Implement auth screens

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:quickreels/controllers/auth_controller.dart';
 
 // COLORS
 const backgroundColor = Colors.black;
@@ -12,3 +13,5 @@ const borderColor = Colors.grey;
 var firebaseAuth = FirebaseAuth.instance;
 var firebaseStorage = FirebaseStorage.instance;
 var firestore = FirebaseFirestore.instance;
+
+var authController = AuthController.instance;
