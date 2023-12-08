fc741984b546d7f047087101d40c4ea217f106a0
Sergio Sánchez <sss4esob@gmail.com>
2023-12-08 14:47:30 +0100

    Implement OnboardingScreen

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -2,12 +2,14 @@ part of 'app_pages.dart';
 
 abstract class Routes {
   Routes._();
+  static const ONBOARDING = _Paths.ONBOARDING;
   static const SIGN_IN = _Paths.SIGN_IN;
   static const HOME = _Paths.HOME;
   static const SIGN_UP = _Paths.SIGN_UP;
 }
 
 abstract class _Paths {
+  static const ONBOARDING = '/onboarding';
   static const SIGN_IN = '/sign-in';
   static const HOME = '/home';
   static const SIGN_UP = '/sign-up';
