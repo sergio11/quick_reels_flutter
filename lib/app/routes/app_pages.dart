d540bc54cade6176155ebdc2d4f68cae97ff81e0
Sergio Sánchez <sss4esob@gmail.com>
2023-12-08 14:47:30 +0100

    Implement OnboardingScreen

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:quickreels/app/features/onboarding/onboarding_screen.dart';
 import 'package:quickreels/app/features/signin/bindings/signin_binding.dart';
 import 'package:quickreels/app/features/signin/views/signin_screen.dart';
 import 'package:quickreels/app/features/signup/bindings/signup_binding.dart';
@@ -9,9 +10,16 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.SIGN_IN;
+  static const INITIAL = Routes.ONBOARDING;
 
   static final routes = [
+    GetPage(
+      name: _Paths.ONBOARDING,
+      page: () => OnBoardingScreen(
+          onSignInPressed: () => Get.offAllNamed(Routes.SIGN_IN),
+          onSignUpPressed: () => Get.offAllNamed(Routes.SIGN_UP)
+      )
+    ),
     GetPage(
       name: _Paths.SIGN_IN,
       page: () => SignInScreen(
