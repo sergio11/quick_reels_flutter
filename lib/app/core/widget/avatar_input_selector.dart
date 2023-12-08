c5f75d7ae2a590a34ad45bbaa4925b5595e8ae85
Sergio Sánchez <sss4esob@gmail.com>
2023-12-08 13:29:54 +0100

    Implement sign up screen

---
+++ lib/app/core/widget/avatar_input_selector.dart
@@ -0,0 +1,74 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:quickreels/app/core/utils/helpers.dart';
+import 'package:quickreels/app/core/values/app_colors.dart';
+
+class AvatarInputSelector extends StatelessWidget {
+  final Uint8List? avatarImageData;
+  final String? currentImageUrl;
+  final VoidCallback onPickUpImageFromGallery;
+
+  const AvatarInputSelector(
+      {super.key,
+      required this.onPickUpImageFromGallery,
+      this.avatarImageData,
+      this.currentImageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      Container(
+          height: 150,
+          width: 150,
+          decoration: BoxDecoration(
+            border: Border.all(color: AppColors.colorPrimary, width: 2),
+            shape: BoxShape.circle,
+            color: AppColors.colorSecondaryExtraLight.withOpacity(0.5),
+          ),
+          alignment: Alignment.center,
+          child: avatarImageData != null
+              ? _buildUserPicture(avatarImageData!)
+              : currentImageUrl != null
+                  ? _buildCurrentUserPicture(currentImageUrl!)
+                  : _buildDefaultAvatarImage()),
+      Positioned(
+        bottom: 0,
+        left: 100,
+        child: Container(
+            height: 40,
+            decoration: BoxDecoration(
+              border: Border.all(color: AppColors.colorSecondaryExtraLight, width: 1),
+              shape: BoxShape.circle,
+              color: AppColors.colorPrimary,
+            ),
+            child: IconButton(
+              onPressed: () => onPickUpImageFromGallery(),
+              icon: const Icon(Icons.add_a_photo,
+                  color: AppColors.colorSecondaryExtraLight),
+            )),
+      )
+    ]);
+  }
+
+  Widget _buildUserPicture(Uint8List avatarImageData) {
+    return CircleAvatar(
+      radius: 100,
+      backgroundImage: MemoryImage(avatarImageData),
+      backgroundColor: AppColors.colorPrimary,
+    );
+  }
+
+  Widget _buildCurrentUserPicture(String profilePictureUrl) {
+    return buildCircleAvatar(
+        imageUrl: profilePictureUrl, radius: 100, showBackgroundColor: false);
+  }
+
+  Widget _buildDefaultAvatarImage() {
+    return const Icon(
+      Icons.person,
+      color: AppColors.colorPrimary,
+      size: 100,
+    );
+  }
+}
