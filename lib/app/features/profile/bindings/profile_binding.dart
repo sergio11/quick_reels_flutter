16e0ab0baa2f1b61c368d586c48c5967bb2d0d1f
Sergio Sánchez <sss4esob@gmail.com>
2023-12-17 12:58:36 +0100

    Implement several improvements at User Profile Screen

--- lib/app/features/profile/bindings/profile_binding.dart
+++ lib/app/features/profile/bindings/profile_binding.dart
@@ -5,7 +5,11 @@ class ProfileBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<ProfileController>(
-      () => ProfileController(getAuthUserUidUseCase: Get.find(), signOutUseCase: Get.find(), getUserDetailsUseCase: Get.find()),
+      () => ProfileController(
+          getAuthUserUidUseCase: Get.find(),
+          signOutUseCase: Get.find(),
+          getUserDetailsUseCase: Get.find(),
+          findReelsByUserUseCase: Get.find()),
     );
   }
 }
--- lib/app/features/profile/controller/profile_controller.dart
+++ lib/app/features/profile/controller/profile_controller.dart
@@ -1,6 +1,9 @@
 import 'package:get/get.dart';
 import 'package:quickreels/app/core/base/base_controller.dart';
 import 'package:quickreels/app/core/base/base_use_case.dart';
+import 'package:quickreels/app/domain/model/reel.dart';
+import 'package:quickreels/app/domain/model/user.dart';
+import 'package:quickreels/app/domain/usecase/find_reels_by_user_use_case.dart';
 import 'package:quickreels/app/domain/usecase/get_auth_user_uid_use_case.dart';
 import 'package:quickreels/app/domain/usecase/get_user_details_use_case.dart';
 import 'package:quickreels/app/domain/usecase/sign_out_use_case.dart';
@@ -10,13 +13,15 @@ class ProfileController extends BaseController<ProfileUiData> {
   final GetAuthUserUidUseCase getAuthUserUidUseCase;
   final SignOutUseCase signOutUseCase;
   final GetUserDetailsUseCase getUserDetailsUseCase;
+  final FindReelsByUserUseCase findReelsByUserUseCase;
 
   static const String USER_UUID_KEY = 'USER_UUID';
 
   ProfileController(
       {required this.getAuthUserUidUseCase,
       required this.signOutUseCase,
-      required this.getUserDetailsUseCase})
+      required this.getUserDetailsUseCase,
+      required this.findReelsByUserUseCase})
       : super(initialUiState: const ProfileUiData());
 
   @override
@@ -36,33 +41,52 @@ class ProfileController extends BaseController<ProfileUiData> {
   }
 
   void signOut() async {
-    callUseCase(
-        signOutUseCase(const DefaultParams()),
-        onComplete: (_) => _handleSignOutCompleted()
-    );
+    callUseCase(signOutUseCase(const DefaultParams()),
+        onComplete: (_) => _handleSignOutCompleted());
   }
 
   void followUser() async {}
 
   void _loadUserDetails(String userUuid) async {
-    _loadUserOrCurrentUserDetails(userUuid, userUuid == await getAuthUserUidUseCase(const DefaultParams()));
+    _loadScreenContentForUser(userUuid,
+        userUuid == await getAuthUserUidUseCase(const DefaultParams()));
   }
 
   void _loadCurrentUserDetails() async {
-    final String authUserUid = await getAuthUserUidUseCase(const DefaultParams());
-    _loadUserOrCurrentUserDetails(authUserUid, true);
+    final String authUserUid =
+        await getAuthUserUidUseCase(const DefaultParams());
+    _loadScreenContentForUser(authUserUid, true);
   }
 
-  void _loadUserOrCurrentUserDetails(String userUuid, bool isAuthUser) async {
+  void _loadScreenContentForUser(String userUuid, bool isAuthUser) async {
     try {
-      final userDetails = await getUserDetailsUseCase(GetUserDetailsParams(userUuid));
-      updateState(uiData.copyWith(userData: userDetails, userUuid: userUuid, isAuthUser: isAuthUser));
+      final userDetails = getUserDetailsUseCase(GetUserDetailsParams(userUuid));
+      final userReels = findReelsByUserUseCase(FindReelsByUserParams(userUuid));
+      callUseCase(Future.wait([userDetails, userReels]), onSuccess: (result) {
+        _handleLoadScreenContentForUser(result, userUuid, isAuthUser);
+      });
     } catch (e) {
       showErrorMessage("An error occurred while loading user details");
     }
   }
+
+  void _handleLoadScreenContentForUser(
+      List result, String userUuid, bool isAuthUser) async {
+    final UserBO? userDetails = result.elementAtOrNull(0) is UserBO
+        ? result.elementAtOrNull(0) as UserBO
+        : null;
+    final List<ReelBO> userReels = result.elementAtOrNull(1) is List<ReelBO>
+        ? result.elementAtOrNull(1) as List<ReelBO>
+        : List.empty();
+
+    updateState(uiData.copyWith(
+        userData: userDetails,
+        userUuid: userUuid,
+        isAuthUser: isAuthUser,
+        reels: userReels));
+  }
+
   void _handleSignOutCompleted() async {
-    print("_handleSignOutCompleted CALLED!");
     logoutController.value = true;
   }
 }
