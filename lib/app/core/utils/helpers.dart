1a67aa742617c66ca3747a01d797e8dbaa14af16
Sergio Sánchez <sss4esob@gmail.com>
2023-12-25 11:20:10 +0100

    Implement image viewer.

--- lib/app/core/utils/helpers.dart
+++ lib/app/core/utils/helpers.dart
@@ -1,26 +1,30 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:quickreels/app/core/utils/utils.dart';
 import 'package:quickreels/app/core/values/app_colors.dart';
 import 'package:quickreels/app/core/widget/animate_gradient_widget.dart';
 import 'package:quickreels/app/core/widget/common_screen_progress_indicator.dart';
 
-Widget buildUserAvatarImage({required String imageUrl, double? radius}) {
-  return Container(
-    decoration: BoxDecoration(
-        border: Border.all(color: AppColors.colorWhite),
-        borderRadius: BorderRadius.circular(radius ?? 22)),
-    child: buildCircleImage(
-        imageUrl: imageUrl, showBackgroundColor: false, radius: radius ?? 22),
+Widget buildUserAvatarImage(
+    {required BuildContext context, required String imageUrl, double? radius}) {
+  return GestureDetector(
+    child: Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: AppColors.colorWhite),
+          borderRadius: BorderRadius.circular(radius ?? 22)),
+      child: buildCircleImage(
+          imageUrl: imageUrl, showBackgroundColor: false, radius: radius ?? 22),
+    ),
+    onTap: () => showImage(context, imageUrl),
   );
 }
 
-Widget buildCircleImage({
-  required String imageUrl,
-  double? radius,
-  bool showBackgroundColor = true,
-  double? progressIndicatorSize = 80
-}) {
+Widget buildCircleImage(
+    {required String imageUrl,
+    double? radius,
+    bool showBackgroundColor = true,
+    double? progressIndicatorSize = 80}) {
   return imageUrl.isEmpty
       ? SizedBox(
           height: progressIndicatorSize,
